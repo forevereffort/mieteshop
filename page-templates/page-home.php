803cca64b46a7ef87ff9258ea0fac7dba051a8df
forevereffort <forevereffort@yandex.com>
2021-06-17 11:24:39 +0800

    working

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -337,6 +337,36 @@
         </div>
     </div>
 </section>
+<section class="home-authors-section">
+    <div class="small-container">
+        <div class="home-authors-title">
+            <h2>ΟΙ ΣΥΓΓΡΑΦΕΙΣ ΤΟΥ ΜΗΝΑ</h2>
+        </div>
+        <div class="home-authors-row">
+            <?php
+                $homepage_authors_of_the_month_rel = get_field('homepage_authors_of_the_month_rel');
+
+                foreach( $homepage_authors_of_the_month_rel as $author ){
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $author->ID ), 'full' );
+            ?>
+                    <div class="home-authors-col">
+                        <div class="home-authors-image">
+                            <img
+                                class="lazyload"
+                                src="<?php echo placeholderImage($image[1], $image[2]); ?>"
+                                data-src="<?php echo aq_resize($image[0], $image[1], $image[2], true); ?>"
+                                alt="<?php echo $author->post_title; ?>">
+                        </div>
+                        <div class="home-authors-name">
+                            <h3><?php echo $author->post_title; ?></h3>
+                        </div>
+                    </div>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</section>
 <section class="home-blog">
     <div class="small-container">
         <div class="home-blog-title">
