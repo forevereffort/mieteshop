081260f15c399a93786b74a0f95989a034baf8ce
forevereffort <forevereffort@yandex.com>
2021-06-18 22:32:17 +0800

    working

---
+++ product/category-product.php
@@ -0,0 +1,38 @@
+<section class="pcat-breadcrumb-section">
+    <div class="content-container">
+        <div class="pcat-breadcrumb-list">
+            <div class="pcat-breadcrumb-item"><a href="#">Βιβλία</a></div>
+            <div class="pcat-breadcrumb-item"><a href="#">Ανθρωπιστικές Επιστήμες</a></div>
+        </div>
+    </div>
+</section>
+<section class="pcat-list-section">
+    <div class="content-container">
+        <div class="pcat-list-title">
+            <h1>Ανθρωπιστικές Επιστήμες</h1>
+        </div>
+        <div class="pcat-list-row">
+            <div class="pcat-list-col">
+                <div class="pcat-list-label">Μετάβαση σε:</div>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Φιλολογία</a>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Ιστορία</a>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Αρχαιολογία</a>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Θρησκεία</a>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Φιλοσοφία</a>
+            </div>
+            <div class="pcat-list-col">
+                <a href="#">Βιογραφίες</a>
+            </div>
+        </div>
+    </div>
+</section>
