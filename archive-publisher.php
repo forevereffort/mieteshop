c6a31e873609eaf7dfd9dfed14fe5b435bee6ebd
forevereffort <forevereffort@yandex.com>
2021-06-21 15:18:02 +0800

    publisher page working

--- archive-publisher.php
+++ archive-publisher.php
@@ -47,4 +47,61 @@
         </div>
     </div>
 </section>
+<div class="archive-publisher-search-section">
+    <div class="content-container">
+        <div class="archive-publisher-search-title">
+            <h2>ΕΥΡΕΤΗΡΙΟ ΕΚΔΟΤΩΝ</h2>
+        </div>
+        <div class="archive-publisher-search-letter-row">
+            <?php
+                $greek_letter_list = ['Α','Β','Γ','Δ','Ε','Ζ','Η','Θ','Ι','Κ','Λ','Μ','Ν','Ξ','O','Π','Ρ','Σ','Τ','Υ','Φ','Χ','Ψ','Ω'];
+                for($i = 0; $i < 24; $i++){
+                    $disable_class = '';
+
+                    if( $i == 7 || $i == 15 || $i == 16 || $i == 20  || $i == 23 ){
+                        $disable_class = 'disable';
+                    }
+            ?>
+                    <div class="archive-publisher-search-letter-col <?php echo $disable_class; ?>"><?php echo $greek_letter_list[$i]; ?></div>
+            <?php
+                }
+            ?>
+        </div>
+        <div class="archive-publisher-search-type-row">
+            <?php
+                $taxonomies = get_terms([
+                    'taxonomy' => 'publisher_type',
+                    'hide_empty' => false
+                ]);
+
+                foreach($taxonomies as $term){
+            ?>
+                    <div class="archive-publisher-search-type-col">
+                        <label><?php echo $term->name; ?><input type="checkbox"><span></span></label>
+                    </div>
+            <?php
+                }
+            ?>
+        </div>
+        <div class="archive-publisher-search-result-list">
+            <?php
+                $args = [
+                    'post_type' => 'publisher',
+                    'posts_per_page' => 30,
+                ];
+            
+                $loop = new WP_Query( $args );
+            
+                while ( $loop->have_posts() ){
+                    $loop->the_post();
+            ?>
+                    <div class="archive-publisher-search-result-col"><a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a></div>
+            <?php
+                }
+
+                wp_reset_query();
+            ?>
+        </div>
+    </div>
+</div>
 <?php get_footer(); ?>
