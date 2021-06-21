651303629edc4d282b21a9de186a690d81e4dc68
forevereffort <forevereffort@yandex.com>
2021-06-21 23:21:45 +0800

    working

--- single-publisher.php
+++ single-publisher.php
@@ -53,6 +53,87 @@
         </div>
     </div>
 </section>
+<section class="single-product-meta-section">
+    <div class="content-container">
+        <div class="single-product-meta-tab-row">
+            <div class="single-product-meta-tab-col">
+                <div class="single-product-meta-tab-item active" data-section-id="video">Video</div>
+            </div>
+            <div class="single-product-meta-tab-col">
+                <div class="single-product-meta-tab-item" data-section-id="article">Σχετικά  Άρθρα</div>
+            </div>
+        </div>
+        <div class="single-product-meta-tab-content-row">
+            <div id="single-product-meta-tab-content--video" class="single-product-meta-tab-content-col">
+                <div class="single-product-video-wrapper" is="mieteshop-product-video-slider">
+                    <div class="swiper-container" data-slider>
+                        <div class="swiper-wrapper">
+                            <?php
+                                for( $i = 0; $i < 5; $i++ ){
+                            ?>
+                                    <div class="swiper-slide">
+                                        <div class="single-product-video-item-row">
+                                            <div class="single-product-video-item-left-col">
+                                                <?php $video_image_url = get_template_directory_uri() . '/assets/images/video.png'; ?>
+                                                <div class="single-product-video-image-wrapper">
+                                                    <img
+                                                        class="lazyload"
+                                                        src="<?php echo placeholderImage(606, 241); ?>"
+                                                        data-src="<?php echo $video_image_url; ?>"
+                                                        alt="video image">
+                                                    <div class="single-product-video-play-icon"><?php include get_template_directory() . '/assets/icons/video-play-icon.svg' ?></div>
+                                                    <div class="single-product-video-resize-icon"><?php include get_template_directory() . '/assets/icons/resize-icon.svg' ?></div>
+                                                </div>
+                                            </div>
+                                            <div class="single-product-video-item-right-col">
+                                                <div class="single-product-video-item-content">
+                                                    <h2>Παρουσίαση της σειράς «ΜΙΝΙΜΑ»</h2>
+                                                    <p>Στο βιβλιοπωλείο του ΜΙΕΤ ( Tσιμισκή 11, Θεσσαλονίκη), πραγματοποιήθηκε η παρουσίαση της σειράς "minima" των εκδόσεων του Μορφωτικού Ιδρύματος Εθνικής Τραπέζης, την Πέμπτη 19 Οκτωβρίου 2017.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="single-product-video-pagination-wrapper" data-pagination></div>
+                </div>
+            </div>
+            <div id="single-product-meta-tab-content--article" class="single-product-meta-tab-content-col hide">
+                <div class="single-product-blog-wrapper" is="mieteshop-product-blog-slider">
+                    <div class="swiper-container" data-slider>
+                        <div class="swiper-wrapper">
+                            <?php
+                                for( $i = 0; $i < 5; $i++ ){
+                            ?>
+                                    <div class="single-product-blog-item swiper-slide">
+                                        <div class="single-product-blog-item-inner">
+                                            <?php $blog_image_url = get_template_directory_uri() . '/assets/images/blog.png'; ?>
+                                            <div class="single-product-blog-image">
+                                                <img
+                                                    class="lazyload"
+                                                    src="<?php echo placeholderImage(399, 261); ?>"
+                                                    data-src="<?php echo $blog_image_url; ?>"
+                                                    alt="video image">
+                                            </div>
+                                            <div class="single-product-blog-content">
+                                                <h2>Ακαδημία Αθηνών Α</h2>
+                                                <p>Δοκιμαστικό κείμενο</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                            <?php
+                                }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <div class="single-product-recently-section">
     <div class="content-container">
         <div class="single-product-recently-title">
@@ -278,6 +359,123 @@
 
                 wp_reset_query();
             ?>
+        </div>
+    </div>
+</section>
+<section class="pcat-results-section">
+    <div class="content-container">
+        <div class="pcat-results-top-title">
+            <h2>ΒΙΒΛΙΑ</h2>
+        </div>
+        <div class="pcat-results-top-row">
+            <div class="pcat-results-top-left-col">
+                <div class="pcat-results-title">
+                    <h2>ΤΙΤΛΟΙ: 32</h2>
+                </div>
+            </div>
+            <div class="pcat-results-top-right-col">
+                <div class="pcat-classification-filter">
+                    <div class="pcat-classification-filter-label pcat-classification-filter-label--black">ΤΑΞΙΝΟΜΗΣΗ</div>
+                    <div class="pcat-classification-filter-select">
+                        <select>
+                            <option value="1">Χρονολογική</option>
+                        </select>
+                        <div class="pcat-classification-filter-select-icon"><?php include get_template_directory() . '/assets/icons/arrow-down-white-icon.svg'; ?></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pcat-results-row">
+            <?php
+                $args = [
+                    'post_type' => 'product',
+                    'posts_per_page' => 16,
+                ];
+            
+                $loop = new WP_Query( $args );
+            
+                while ( $loop->have_posts() ){
+                    $loop->the_post();
+                    global $product;
+
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'full' );
+                    $authors = get_field('book_contributors_syggrafeas', $product->get_id());
+            ?>
+                    <div class="pcat-results-col">
+                        <div class="pcat-result-item">
+                            <div class="pcat-result-item-info">
+                                <div class="pcat-result-item-image">
+                                    <img
+                                        class="lazyload"
+                                        src="<?php echo placeholderImage($image[1], $image[2]); ?>"
+                                        data-src="<?php echo aq_resize($image[0], $image[1], $image[2], true); ?>"
+                                        alt="<?php echo $product->get_name(); ?>">
+                                </div>
+                                <div class="pcat-result-item-meta-row">
+                                    <div class="pcat-result-item-meta-col">
+                                        <div class="pcat-result-item-favorite">
+                                            <a href="#"><span><?php include get_template_directory() . '/assets/icons/favorite-small-icon.svg' ?></span></a>
+                                        </div>
+                                    </div>
+                                    <div class="pcat-result-item-meta-col">
+                                        <div class="pcat-result-item-busket">
+                                            <a href="#"><span><?php include get_template_directory() . '/assets/icons/busket-small-icon.svg' ?></span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                                    if( !empty($authors) ){
+                                        echo '<div class="pcat-result-item-author-list">';
+                                        if( count($authors) > 3 ){
+                                            echo '<div class="pcat-result-item-author-item">Συλλογικό Έργο</div>';
+                                        } else {
+                                            foreach( $authors as $author ){
+                                                echo '<div class="pcat-result-item-author-item"><a href="'. get_permalink($author->ID) . '">' . $author->post_title . '</a></div>';
+                                            }
+                                        }
+                                        echo '</div>';
+                                    }
+                                ?>
+                                <div class="pcat-result-item-title"><h3><?php echo $product->get_name(); ?></h3></div>
+                            </div>
+                            <div class="pcat-result-item-footer-row">
+                                <div class="pcat-result-item-footer-col">
+                                    <div class="pcat-result-item-footer-product-price">
+                                        <?php echo $product->get_price_html(); ?>
+                                    </div>
+                                </div>
+                                <div class="pcat-result-item-footer-col">
+                                    <div class="pcat-result-item-footer-product-discount">-30%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            <?php
+                }
+                wp_reset_query();
+            ?>
+        </div>
+        <div class="pcat-results-footer-options">
+            <div class="pcat-results-footer-options-col">
+                <div class="pcat-results-navigation">
+                    <div class="pcat-results-navigation-row">
+                        <div class="pcat-results-navigation-item active"><a href="#">1</a></div>
+                        <div class="pcat-results-navigation-item"><a href="#">2</a></div>
+                        <div class="pcat-results-navigation-next"><?php include get_template_directory() . '/assets/icons/arrow-right-icon.svg' ?></div>
+                    </div>
+                </div>
+            </div>
+            <div class="pcat-results-footer-options-col">
+                <div class="pcat-results-footer-select">
+                    <div class="pcat-results-footer-select-label">Mετάβαση στη σελίδα</div>
+                    <div class="pcat-results-footer-select-elem">
+                        <select>
+                            <option value="1">1</option>
+                        </select>
+                        <div class="pcat-results-footer-select-elem-icon"><?php include get_template_directory() . '/assets/icons/arrow-down-icon.svg'; ?></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
