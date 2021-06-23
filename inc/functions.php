f688d6a922039e1919bee167774b0bbb76d02b36
forevereffort <forevereffort@yandex.com>
2021-06-23 10:28:31 +0800

    enable gutenberg only for post

--- inc/functions.php
+++ inc/functions.php
@@ -1,6 +1,14 @@
 <?php
 // Disable Gutenberg
-add_filter( 'use_block_editor_for_post', '__return_false' );
+// add_filter( 'use_block_editor_for_post_type', '__return_false' );
+
+add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
+function prefix_disable_gutenberg($current_status, $post_type){
+  // Use your post type key instead of 'product'
+  if ($post_type === 'post') return $current_status;
+  
+  return false;
+}
 
 /**
  * Removes Gutenberg default styles on front-end
