bf8d480bfbb73aebf43b266fdcb7e9f4c28fb175
forevereffort <forevereffort@yandex.com>
2021-07-24 13:48:29 +0800

    work

--- page-templates/page-my-account.php
+++ page-templates/page-my-account.php
@@ -12,12 +12,7 @@
 ?>
 <section class="my-account-section">
     <div class="content-container">
-        <div class="my-account-icon">
-            <span><?php include get_template_directory() . '/assets/icons/my-account-icon.svg'; ?></span>
-        </div>
-        <div class="my-account-inner">
-            <?php the_content(); ?>
-        </div>
+        <?php the_content(); ?>
     </div>
 </section>
 <?php
