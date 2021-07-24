b51e8bb8067a5fe8af471780af33dc218b82c91e
forevereffort <forevereffort@yandex.com>
2021-07-24 14:17:46 +0800

    work

--- page-templates/page-my-account.php
+++ page-templates/page-my-account.php
@@ -6,6 +6,18 @@
 <?php get_header(); ?>
 
 <?php
+    if( is_user_logged_in() ){
+        $endpoint = WC()->query->get_current_endpoint();  
+        $endpoint_title = WC()->query->get_endpoint_title( $endpoint );
+?>
+        <section class="my-account-title-section">
+            <div class="content-container">
+                <h1><?php echo $endpoint_title; ?></h1>
+            </div>
+        </section>
+<?php
+    }
+    
     if ( have_posts() ) {
         while ( have_posts() ){
             the_post();
