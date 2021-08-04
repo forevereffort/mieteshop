824d56ac2aef9cf35b700043d7cc86671a28c6a4
forevereffort <forevereffort@yandex.com>
2021-08-04 10:14:39 +0800

    single product page working

---
+++ product/sections/single-product-meta.php
@@ -0,0 +1,154 @@
+<?php
+    global $product;
+    
+    if( have_rows('book_reviews') || have_rows('book_audio_repeater') || have_rows('book_videos') || get_field('book_related_articles')  ) {
+?>
+        <section class="single-product-meta-section">
+            <div class="content-container">
+                <div class="single-product-meta-tab-row">
+                    <div class="single-product-meta-tab-col">
+                        <div class="single-product-meta-tab-item active" data-section-id="review">Βιβλιοκρισίες</div>
+                    </div>
+                    <div class="single-product-meta-tab-col">
+                        <div class="single-product-meta-tab-item" data-section-id="audio">Audio</div>
+                    </div>
+                    <div class="single-product-meta-tab-col">
+                        <div class="single-product-meta-tab-item" data-section-id="video">Video</div>
+                    </div>
+                    <div class="single-product-meta-tab-col">
+                        <div class="single-product-meta-tab-item" data-section-id="article">Σχετικά  Άρθρα</div>
+                    </div>
+                </div>
+                <div class="single-product-meta-tab-content-row">
+                    <div id="single-product-meta-tab-content--review" class="single-product-meta-tab-content-col">
+                        <div class="single-product-review-wrapper" is="mieteshop-product-review-slider">
+                            <div class="swiper-container" data-slider>
+                                <div class="swiper-wrapper">
+                                    <?php
+                                        if( have_rows('book_reviews') ){ 
+                                            while( have_rows('book_reviews') ){
+                                                the_row();    
+                                    ?>
+                                                <div class="swiper-slide">
+                                                    <div class="single-product-review">
+                                                        <div class="single-product-review__content">
+                                                            <p><?php echo get_sub_field('book_reviews_text'); ?></p>
+                                                        </div>
+                                                        <div class="single-product-review__autor"><a href="<?php echo get_sub_field('book_reviews_source_link'); ?>"><?php echo get_sub_field('book_reviews_source_text'); ?></a></div>
+                                                    </div>
+                                                </div>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="single-product-review-pagination-wrapper" data-pagination></div>
+                        </div>
+                    </div>
+                    <div id="single-product-meta-tab-content--audio" class="single-product-meta-tab-content-col hide">
+                        <div class="single-product-audio-wrapper" is="mieteshop-product-audio-slider">
+                            <div class="swiper-container" data-slider>
+                                <div class="swiper-wrapper">
+                                    <?php
+                                        if( have_rows('book_audio_repeater') ){ 
+                                            while( have_rows('book_audio_repeater') ){
+                                                the_row();
+                                    ?>
+                                                <div class="swiper-slide">
+                                                    <div class="single-product-audio-row">
+                                                        <div class="single-product-audio__content">
+                                                            <?php echo get_sub_field('book_audio_embed_code'); ?>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="single-product-audio-pagination-wrapper" data-pagination></div>
+                        </div>
+                    </div>
+                    <div id="single-product-meta-tab-content--video" class="single-product-meta-tab-content-col hide">
+                        <div class="single-product-video-wrapper" is="mieteshop-product-video-slider">
+                            <div class="swiper-container" data-slider>
+                                <div class="swiper-wrapper">
+                                    <?php
+                                        if( have_rows('book_videos') ) { 
+                                            while( have_rows('book_videos') ){
+                                                the_row();
+
+                                                // get_sub_field('book_video_embed_code');
+                                                $video_image_url = get_sub_field('book_video_cover_image');
+                                    ?>
+                                                <div class="swiper-slide">
+                                                    <div class="single-product-video-item-row">
+                                                        <div class="single-product-video-item-left-col">
+                                                            <div class="single-product-video-image-wrapper">
+                                                                <img
+                                                                    class="lazyload"
+                                                                    src="<?php echo placeholderImage(606, 241); ?>"
+                                                                    data-src="<?php echo aq_resize($video_image_url['url'], 606, 241, true); ?>"
+                                                                    alt="video image">
+                                                                <div class="single-product-video-play-icon"><?php include get_template_directory() . '/assets/icons/video-play-icon.svg' ?></div>
+                                                                <div class="single-product-video-resize-icon"><?php include get_template_directory() . '/assets/icons/resize-icon.svg' ?></div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="single-product-video-item-right-col">
+                                                            <div class="single-product-video-item-content">
+                                                                <h2><?php echo get_sub_field('book_video_title'); ?></h2>
+                                                                <?php echo apply_filters('the_content', get_sub_field('book_video_description')); ?>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                    <?php
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                            <div class="single-product-video-pagination-wrapper" data-pagination></div>
+                        </div>
+                    </div>
+                    <div id="single-product-meta-tab-content--article" class="single-product-meta-tab-content-col hide">
+                        <div class="single-product-blog-wrapper" is="mieteshop-product-blog-slider">
+                            <div class="swiper-container" data-slider>
+                                <div class="swiper-wrapper">
+                                    <?php
+                                        $related_articles = get_field('book_related_articles');
+                                        foreach($related_articles as $article) {
+                                            $blog_image_url = wp_get_attachment_url( get_post_thumbnail_id($article->ID) );
+                                    ?>
+                                            <div class="single-product-blog-item swiper-slide">
+                                                <div class="single-product-blog-item-inner">
+                                                    <div class="single-product-blog-image">
+                                                        <a href="<?php echo get_permalink($article->ID); ?>">
+                                                            <img
+                                                                class="lazyload"
+                                                                src="<?php echo placeholderImage(399, 261); ?>"
+                                                                data-src="<?php echo aq_resize($blog_image_url['url'], 399, 261, true); ?>"
+                                                                alt="video image">
+                                                        </a>
+                                                    </div>
+                                                    <div class="single-product-blog-content">
+                                                        <h2><a href="<?php echo get_permalink($article->ID); ?>"><?php echo $article->post_title; ?></a></h2>
+                                                        <?php echo get_field('post_lead', $article->ID); //echo apply_filters('the_content', $article->post_excerpt); ?>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    <?php
+                                        }
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php
+    }
+?>
---
+++ product/sections/single-product-realted.php
@@ -0,0 +1,90 @@
+<?php
+    global $product;
+    
+    $args = [
+       'post_type' => 'product',
+       'posts_per_page' => 16,
+       'orderby' => 'rand',
+    ];
+
+    $related_posts = new WP_Query( $args );
+    
+    if ( $related_posts->have_posts() ) {
+?>
+        <section class="single-product-realted-section">
+            <div class="content-container">
+                <div class="single-product-realted-title">
+                    <h2>ΣΧΕΤΙΚΟΙ ΤΙΤΛΟΙ</h2>
+                </div>
+                <div class="pcat-results-row">
+                    <?php
+                        while ( $related_posts->have_posts() ){
+                            $related_posts->the_post();
+
+                            global $product;
+
+                            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'full' );
+                                $authors = get_field('book_contributors_syggrafeas', $product->get_id());
+                    ?>
+                            <div class="pcat-results-col">
+                                <div class="pcat-result-item">
+                                    <div class="pcat-result-item-info">
+                                        <div class="pcat-result-item-image">
+                                            <a href="<?php echo get_permalink($product->get_id()); ?>">
+                                                <img
+                                                    class="lazyload"
+                                                    src="<?php echo placeholderImage($image[1], $image[2]); ?>"
+                                                    data-src="<?php echo aq_resize($image[0], $image[1], $image[2], true); ?>"
+                                                    alt="<?php echo $product->get_name(); ?>">
+                                            </a>
+                                        </div>
+                                        <div class="pcat-result-item-meta-row">
+                                            <div class="pcat-result-item-meta-col">
+                                                <div class="pcat-result-item-favorite">
+                                                    <a href="#"><span><?php include get_template_directory() . '/assets/icons/favorite-small-icon.svg' ?></span></a>
+                                                </div>
+                                            </div>
+                                            <div class="pcat-result-item-meta-col">
+                                                <div class="pcat-result-item-busket">
+                                                <a class="js-mieteshop-add-to-cart" href="#" data-quantity="1" data-product_id="<?php echo $product->get_id(); ?>" data-variation_id="0" data-product_sku="<?php echo $product->get_sku(); ?>"><span><?php include get_template_directory() . '/assets/icons/busket-small-icon.svg' ?></span></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php
+                                            if( !empty($authors) ){
+                                                echo '<div class="pcat-result-item-author-list">';
+                                                if( count($authors) > 3 ){
+                                                    echo '<div class="pcat-result-item-author-item">Συλλογικό Έργο</div>';
+                                                } else {
+                                                    foreach( $authors as $author ){
+                                                        echo '<div class="pcat-result-item-author-item"><a href="'. get_permalink($author->ID) . '">' . $author->post_title . '</a></div>';
+                                                    }
+                                                }
+                                                echo '</div>';
+                                            }
+                                        ?>
+                                        <div class="pcat-result-item-title"><h3><a href="<?php echo get_permalink($product->get_id()); ?>"><?php echo $product->get_name(); ?></a></h3></div>
+                                    </div>
+                                    <div class="pcat-result-item-footer-row">
+                                        <div class="pcat-result-item-footer-col">
+                                            <div class="pcat-result-item-footer-product-price">
+                                                <?php echo $product->get_price_html(); ?>
+                                            </div>
+                                        </div>
+                                        <!--div class="pcat-result-item-footer-col">
+                                            <div class="pcat-result-item-footer-product-discount">-30%</div>
+                                        </div-->
+                                    </div>
+                                </div>
+                            </div>
+                    <?php
+                        }
+                        
+                        wp_reset_query();
+                    ?>
+                </div>
+            </div>
+        </section>
+<?php
+    }
+?>
