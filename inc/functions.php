dcbb9bc587cc987c8213196276047cbbe2aace38
forevereffort <forevereffort@yandex.com>
2021-10-11 23:15:23 +0800

    working

--- inc/functions.php
+++ inc/functions.php
@@ -127,7 +127,6 @@ include_once 'woo-functions.php';
 include_once 'aq-resizer.php';
 include_once 'helper.php';
 
-include_once 'custom-post-types/publisher.php';
 include_once 'custom-post-types/contributor.php';
 
 include_once 'page-functions/category-product-function.php';
