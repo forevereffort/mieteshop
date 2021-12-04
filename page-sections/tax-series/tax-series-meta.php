9baccb6843993fcf25ea349d52ba8672301767b3
forevereffort <forevereffort@yandex.com>
2021-12-04 12:28:20 +0800

    series taxonomy page remove video and sampling when the data is empty

--- page-sections/tax-series/tax-series-meta.php
+++ page-sections/tax-series/tax-series-meta.php
@@ -1,25 +1,26 @@
 <?php
     $current_series_taxonomy = get_queried_object();
+
+    if( have_rows('series_videos', 'series_'.$current_series_taxonomy->term_id) ) { 
 ?>
-<section class="single-product-meta-section" is="mieteshop-series-meta-section">
-    <div class="content-container">
-        <div class="single-product-meta-tab-row">
-            <div class="single-product-meta-tab-col">
-                <div class="single-product-meta-tab-item active">Video</div>
+    <section class="single-product-meta-section" is="mieteshop-series-meta-section">
+        <div class="content-container">
+            <div class="single-product-meta-tab-row">
+                <div class="single-product-meta-tab-col">
+                    <div class="single-product-meta-tab-item active">Video</div>
+                </div>
             </div>
-        </div>
-        <div class="single-product-meta-tab-content-row"  >
-            <div id="single-product-meta-tab-content--video" class="single-product-meta-tab-content-col">
-                <div class="single-product-video-wrapper" is="mieteshop-series-video-slider">
-                    <div class="swiper-container" data-video-slider>
-                        <div class="swiper-wrapper">
-                            <?php
-                                if( have_rows('series_videos', 'series_'.$current_series_taxonomy->term_id) ) { 
+            <div class="single-product-meta-tab-content-row"  >
+                <div id="single-product-meta-tab-content--video" class="single-product-meta-tab-content-col">
+                    <div class="single-product-video-wrapper" is="mieteshop-series-video-slider">
+                        <div class="swiper-container" data-video-slider>
+                            <div class="swiper-wrapper">
+                                <?php
                                     while( have_rows('series_videos', 'series_'.$current_series_taxonomy->term_id) ){
                                         the_row();
                                         
                                         $video_image_url = get_sub_field('contributor_video_cover_image');
-                            ?>
+                                ?>
                                         <div class="swiper-slide">
                                             <div class="single-product-video-item-row">
                                                 <div class="single-product-video-item-left-col">
@@ -42,22 +43,24 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <?php
-                                    }
-                                }                         
-                            ?>
+                                <?php
+                                    }                         
+                                ?>
+                            </div>
                         </div>
+                        <div class="single-product-video-pagination-wrapper" data-video-pagination></div>
                     </div>
-                    <div class="single-product-video-pagination-wrapper" data-video-pagination></div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
-<div id="js-sc-video-popup" class="single-contributor-video-popup hide">
-    <div id="js-sc-video-popup__close-btn" class="single-contributor-video-popup__close-btn"></div>
-    <div class="single-contributor-video-popup__video">
-        <div id="js-sc-video-popup__video-wrapper">
+    </section>
+    <div id="js-sc-video-popup" class="single-contributor-video-popup hide">
+        <div id="js-sc-video-popup__close-btn" class="single-contributor-video-popup__close-btn"></div>
+        <div class="single-contributor-video-popup__video">
+            <div id="js-sc-video-popup__video-wrapper">
+            </div>
         </div>
     </div>
-</div>
+<?php
+    }
+?>
